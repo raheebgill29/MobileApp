20bd2291c2702a0d2c4c171f26bc7a3203eedbf8
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2024-06-08 13:15:50 +0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Canvas } from "@react-three/fiber"
 import MainContainer from "./MainContainer"
+import { OrbitControls } from "@react-three/drei"
 
 
 function App() {
 
-  return <Canvas>
+  return <Canvas camera={{fov:75,near:0.1,far:1000,position:[0,3,3]}}>
+     <OrbitControls enablePan={true} enableZoom={true} />
     <MainContainer/>
   </Canvas>
     
