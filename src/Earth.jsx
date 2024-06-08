d85e1ca8163b35af826c50fa55257ab2bac5684c
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2024-06-08 13:16:33 +0500

    Earth

--- src/Earth.jsx
+++ src/Earth.jsx
@@ -1,12 +1,34 @@
 import { useTexture } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+import Moon from "./Moon"
 const Earth = () => {
-    const [earthTexture] = useTexture(['public/assets/earth.jpg'])
+    const earthRef=useRef()
+    const [earthTexture,earthNormalMap,earthSpecularMap,earthDisplacement] = useTexture(['assets/earth.jpg','assets/2k_earth_normal_map.jpg','assets/2k_earth_specular_map.jpg','assets/earthDisplacementMap.jpg'])
+    useFrame(() => {
+        earthRef.current.rotation.y +=0.001    
+    }
+
+    )
     return(
-    <mesh>
+        <group>
+    <mesh ref={earthRef}>
         // Radius , x-axis , y-axis
         <sphereGeometry args={[1,32,32]}/>
-        <meshStandardMaterial map={earthTexture}/>
+        {/* <meshStandardMaterial 
+        map={earthTexture}
+        normalMap={earthNormalMap}/> */}
+         <meshPhongMaterial 
+        map={earthTexture}
+        normalMap={earthNormalMap}
+        specularMap={earthSpecularMap}
+        shininess={10}
+        displacementMap={earthDisplacement}
+        displacementScale={0.01}
+        />
     </mesh>
+    <Moon/>
+    </group>
     )
 }
 
